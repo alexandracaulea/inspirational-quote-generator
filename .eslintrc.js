e2c7b95e552540005090dcccf3073735989210e2
Alexandra Caulea <alexandra.caulea@gmail.com>
2020-09-26 18:58:11 +0300

    Add airbnb style guide and eslint configuration

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  env: {
+    browser: true,
+  },
+  extends: ['plugin:react/recommended', 'airbnb'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+      ecmaVersion: 2021,
+    },
+    sourceType: 'module',
+  },
+  plugins: ['react'],
+  rules: {
+    'linebreak-style': ['error', 'windows'],
+  },
+};
